b73ec5effdf529aec3c960175f52fcda48ecb964
Dakshesh <dpasala@calpoly.edu>
2026-02-12 16:25:22 -0800

    just have the initial thing setup

--- memSim.py
+++ memSim.py
@@ -3,39 +3,50 @@
 from collections import deque
 import sys
 
-def main():
+page_table = []
+tlb = None
+physical_memory = []
+frame_to_page = []
+free_frames = []
+last_used = {}
+time_counter = 0
 
-    if len(sys.argv) < 2:
-        print("usage: ./memSim <referenceFile.txt> [frames] [pra]")
-        sys.exit(1)
+total_addresses = page_faults = tlb_hits = tlb_misses= 0
+
+frames = 256
+pra = ""
+backing_store = None
+
+def get_page_offset(logical_address):
+    page = (logical_address >> 8) & 0xFF
+    offset = logical_address & 0xFF
+    return page, offset
+
+def main():
+    global page_table, tlb, physical_memory
+    global frame_to_page, free_frames
+    global frames, pra, backing_store
+
 
     reference_file = sys.argv[1]
-
-    frames = 256
-    pra = "FIFO"
-
-    if len(sys.argv) >= 3:
-        frames = int(sys.argv[2])
-
-    if len(sys.argv) >= 4:
-        pra = sys.argv[3]
+    frames = int(sys.argv[2])
+    pra = sys.argv[3]
 
     if frames <= 0 or frames > 256:
-        print("frames must be between 1 and 256")
+        print("frames are wrong try again")
         sys.exit(1)
-
+    
     if pra not in ["FIFO", "LRU", "OPT"]:
-        print("pra must be FIFO, LRU, or OPT")
+        print("wrong algorithm cuh")
         sys.exit(1)
-
-    page_table_size = 28
+    
+    # okay page_table
     page_table = [
         {"frame": None, "present": False}
-        for _ in range(page_table_size)
+        for _ in range(256)
     ]
 
-    tlb_size = 16
-    tlb = deque(maxlen=tlb_size)
+    tlb = deque(maxlen=16)
 
     physical_memory = [
         bytearray(256)
@@ -45,29 +56,13 @@ def main():
     frame_to_page = [None] * frames
     free_frames = list(range(frames))
 
-    last_used = {}
-    time_counter = 0
-
-    reference_list = []
-
-    total_addresses = 0
-    page_faults = 0
-    tlb_hits = 0
-    tlb_misses = 0
+    backing_store = open("BACKING_STORE.bin", "rb")
 
     with open(reference_file, "r") as f:
         for line in f:
             line = line.strip()
             if line:
-                reference_list.append(int(line))
+                logical_address = int(line)
+                page, offset = get_page_offset(logical_address)
 
-    backing_store = open("BACKING_STORE.bin", "rb")
-
-    print("Initialization complete.")
-    print(f"Frames: {frames}")
-    print(f"PRA: {pra}")
-    print(f"Loaded {len(reference_list)} addresses")
-
-
-if __name__ == "__main__":
-    main()
+                print(f"logical address: {logical_address} maps to page: {page} and offset: {offset}")
