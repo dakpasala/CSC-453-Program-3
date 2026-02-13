b38938aa59bb9d1f0288f95184b0d53b94f215b4
Dakshesh <dpasala@calpoly.edu>
2026-02-12 17:07:14 -0800

    the values print well

--- memSim.py
+++ memSim.py
@@ -17,6 +17,63 @@ frames = 256
 pra = ""
 backing_store = None
 
+
+# ------------ TLB CRAP ------------
+def tlb_lookup(page):
+    for tlb_page, tlb_frame in tlb:
+        if tlb_page == page:
+            return tlb_frame
+    
+    return None
+
+def tlb_insert(page, frame):
+    tlb.append((page, frame))
+
+def translate_address(logical_address):
+    global page_faults, tlb_hits, tlb_misses
+    
+    # get the page and offset idk if we need offset
+    page, offset = get_page_offset(logical_address)
+    
+    # check if it's in the TLB
+    frame = tlb_lookup(page)
+
+    if frame is not None: tlb_hits += 1
+    else:
+        tlb_misses += 1
+
+        # check if page table is present or not, im not sure if this is correct or not, we can fix tho
+        if page_table[page]["present"]:
+            frame = page_table[page]["frame"]
+        else:
+            page_faults += 1
+            
+            if free_frames: frame = free_frames.pop(0)
+            #else:
+                # this will be FIFO, OPT we gotta implement later on unfort
+
+                
+
+
+
+            backing_store.seek(page * 256)
+            data = backing_store.read(256)
+
+            physical_memory[frame] = bytearray(data)
+
+            page_table[page]["frame"] = frame
+            page_table[page]["present"] = True
+            frame_to_page[frame] = page
+        
+        tlb_insert(page, frame)
+    
+    physical_address = frame * 256 + offset
+    value = physical_memory[frame][offset]
+
+    return frame, physical_address, value
+
+# ------------------------------------
+
 def get_page_offset(logical_address):
     page = (logical_address >> 8) & 0xFF
     offset = logical_address & 0xFF
@@ -27,10 +84,13 @@ def main():
     global frame_to_page, free_frames
     global frames, pra, backing_store
 
+    print("program started")
 
     reference_file = sys.argv[1]
+    if reference_file: print("good file")
     frames = int(sys.argv[2])
     pra = sys.argv[3]
+    if len(pra) > 0: print("pra good")
 
     if frames <= 0 or frames > 256:
         print("frames are wrong try again")
@@ -60,9 +120,13 @@ def main():
 
     with open(reference_file, "r") as f:
         for line in f:
+            print(line)
             line = line.strip()
             if line:
                 logical_address = int(line)
-                page, offset = get_page_offset(logical_address)
+                frame, physical_address, value = translate_address(logical_address)
 
-                print(f"logical address: {logical_address} maps to page: {page} and offset: {offset}")
+                print(f"logical address: {logical_address} maps to frame {frame}, value: {value}, and physical_address: {physical_address}")
+
+if __name__ == "__main__":
+    main()
